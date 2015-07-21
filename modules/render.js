2cb720fa7345acc8f66f87feb6a73e451d43c914
Louis Capitanchik <ljcapitanio@gmail.com>
2015-07-21 11:24:10 +0100

    Update to latest Sideburns

--- modules/render.js
+++ modules/render.js
@@ -1,9 +1,20 @@
 var path = require("path"),
     fs = require("fs"),
     sideburns = require("bp-sideburns"),
+    q = require("q"),
     render = function(basepath, filepath, data, options){
-        var file = fs.readFileSync(path.join(basepath, filepath));
-        this.send(sideburns.render(file, data, options), {'Content-Type': 'text/html'});
+        var extension = options.extension || ".sb",
+            def = q.defer(),
+            file = fs.readFile(path.join(basepath, filepath), function(e, f){
+                if(e) {
+                    def.reject(e);
+                } else {
+                    this.send(200, sideburns(f, data, options, {'Content-Type': 'text/html'})).then(function(d){
+                        def.resolve(true);
+                    })
+                }
+            });
+        return def.promise;
     }
 
 module.exports = function(basepath) {
