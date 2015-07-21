4524cd1ca9ece8f899346a32366ea3da7b520bb3
Louis Capitanchik <ljcapitanio@gmail.com>
2015-07-21 14:07:27 +0100

    NBF render function implementation change

--- modules/render.js
+++ modules/render.js
@@ -11,7 +11,11 @@ var path = require("path"),
             def = q.defer(),
             file = fs.readFile(path.join(basepath, filepath) + extension, function(e, f){
                 if(e) {
-                    def.reject(e);
+                    if(e.message.indexOf("ENOENT") > -1) {
+                        def.resolve(false); // ENOENT represents no file found;
+                    } else {
+                        def.reject(e);
+                    }
                 } else {
                     res.send(200, sideburns(f.toString(), data, options), {'Content-Type': 'text/html'});
                     def.resolve(true);
@@ -22,6 +26,19 @@ var path = require("path"),
 
 module.exports = function(basepath) {
     return function(req, res){
-        res.render = render.bind(res, basepath);
+        res.render = function(filepath, data, options) {
+            var def = q.defer();
+            //Intercept render promise and send an error if rendering failed,
+            //Either way provide a better formed promise result
+            render.call(res, basepath, filepath, data, options).then(function(d) {
+                if(d) {
+                    def.resolve([req, res]);
+                } else {
+                    res.error(500, {code: 500, message: "Badly configured template request", details: {req: req, res: res}});
+                    def.resolve([res, res]);
+                }
+            });
+            return def.promise;
+        };
     }
 }
