b9bee5a6119ff10febe9609d2270637efae70132
Louis Capitanchik <ljcapitanio@gmail.com>
2015-07-23 12:46:20 +0100

    Router now send 405 on method not supported, wont crash server

--- modules/router.js
+++ modules/router.js
@@ -1,5 +1,14 @@
 var mwrouter = function (router, req, res, next) {
-    return router.route(req.method, req.url, req, res);
+    var val = null;
+    try {
+        val = router.route(req.method, req.url, req, res);
+    } catch (e) {
+        if(e.message.indexOf("Router has no METHOD") > -1) {
+            res.error();
+        } else {
+            throw e;
+        }
+    }
 }
 
 module.exports = function(router){
